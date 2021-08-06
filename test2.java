b30a0cc95e3a537942faa37520751a70da50956a
rittiroj <64120127+rittiroj@users.noreply.github.com>
2021-08-06 09:50:09 +0700

    Add files via upload

--- test2.java
+++ test2.java
@@ -5,13 +5,13 @@ public class test2{
     }
     
     public static void findChar(String sentence, String character) {
-       int index = 0;
+       int str = 0;
         for (int i = 1; i <= sentence.length(); i++) {
             if (sentence.substring(i-1, i).equals(character)) {
-                index = i;
+                str = i;
             }
         }
-        System.out.println("result: " + index);
+        System.out.println("result: " + str);
         
     }
 }
